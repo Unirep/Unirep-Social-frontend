ecfa911e59441a08af801d7844948507a9edd9fa
yu-zhen <wlphg010688@gmail.com>
2022-02-15 17:15:10 +0800

    remove join on banner while user is already login

--- src/components/mainPage/banner.tsx
+++ src/components/mainPage/banner.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react';
 import { ABOUT_URL } from '../../config';
+import { WebContext } from '../../context/WebContext';
 
 type Props = {
     closeBanner: () => void
 }
 
 const Banner = ({ closeBanner }: Props) => {
+    const { user } = useContext(WebContext);
     return (
         <div className="banner-row">
             <div className="banner">
@@ -13,9 +16,9 @@ const Banner = ({ closeBanner }: Props) => {
                 <div className="banner-content">Stay up to date & share all things about Ethereum.</div>
                 <div className="banner-buttons">
                     <a className="banner-button" href={ABOUT_URL}>How it work?</a>
-                    <a className="banner-button" href="/signup">Join now</a>
+                    {user === null? <a className="banner-button" href="/signup">Join now</a> : <div></div>}
                 </div>
-                <div className="banner-close" onClick={closeBanner}><img src="/images/close.png" /></div>
+                <div className="banner-close" onClick={closeBanner}><img src="/images/close.svg" /></div>
             </div>
         </div>
     );
