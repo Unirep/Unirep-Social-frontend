a62a687cc087ccbdb3db0fc94cffc4413f3f4487
Chance Hudson <jchancehud@gmail.com>
2022-04-12 08:44:54 -0500

    fix: remove unnecessary startDaemon calls

--- src/context/Queue.ts
+++ src/context/Queue.ts
@@ -55,9 +55,7 @@ export class Queue {
         }
     }
 
-    async load() {
-        this.startDaemon()
-    }
+    async load() {}
 
     get isLoading() {
         return this.loadingState === LoadingState.loading
@@ -89,7 +87,6 @@ export class Queue {
             ...options,
         })
         // TODO: possibly auto queue a UST if needed?
-        this.startDaemon()
     }
 
     resetLoading() {
