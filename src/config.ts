3011f5ee191c0a19b2ce8a8b4c1e3a207f2e4375
yu-zhen <wlphg010688@gmail.com>
2022-04-12 14:55:35 +0800

    add server and eth provider to .env file

--- src/config.ts
+++ src/config.ts
@@ -4,14 +4,9 @@ import Unirep from '@unirep/contracts/artifacts/contracts/Unirep.sol/Unirep.json
 
 const EXPLORER_URL = 'https://kovan-optimistic.etherscan.io'
 
-const SERVER = 'https://unirep.tubby.cloud'
-// const SERVER = 'http://localhost:5000'
-// const DEFAULT_ETH_PROVIDER_URL = 'ws://localhost:8545'
-// const DEFAULT_ETH_PROVIDER_URL =
-//     'https://arb-rinkeby.g.alchemy.com/v2/LzkSzVuFVZimTOB45xcCH3Cals4dTzk_'
+const SERVER = process.env.SERVER ?? 'http://localhost:3001'
 const DEFAULT_ETH_PROVIDER_URL =
-    'https://opt-kovan.g.alchemy.com/v2/b5eaS0X3OMk54IppGh9ApffGoIOLIHOU'
-// const DEFAULT_ETH_PROVIDER_URL = 'http://localhost:8545'
+    process.env.DEFAULT_ETH_PROVIDER ?? 'http://localhost:8545'
 const DEFAULT_ETH_PROVIDER = DEFAULT_ETH_PROVIDER_URL.startsWith('http')
     ? new ethers.providers.JsonRpcProvider(DEFAULT_ETH_PROVIDER_URL)
     : new ethers.providers.WebSocketProvider(DEFAULT_ETH_PROVIDER_URL)
