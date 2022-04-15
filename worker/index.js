c7cb182cf31bb2035656f9ee4b04b09aeafee013
Chance Hudson <jchancehud@gmail.com>
2022-04-15 16:58:38 -0500

    feat: proxy key requests to unirep backend

--- worker/index.js
+++ worker/index.js
@@ -1,8 +1,10 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
 // Enables edge cdn - https://developers.cloudflare.com/workers/learning/how-the-cache-works/
 const DEBUG = true
 const ENABLE_ASSET_CACHE = false
+const ENABLE_KEY_CACHE = true
+const BACKEND_URL = 'https://unirep.tubby.cloud'
 
 addEventListener('fetch', (event) => {
     event.respondWith(generateResponse(event))
@@ -10,8 +12,37 @@ addEventListener('fetch', (event) => {
 
 async function generateResponse(event) {
     // https://www.npmjs.com/package/@cloudflare/kv-asset-handler#optional-arguments
+    const url = new URL(event.request.url)
+    if (url.pathname.startsWith('/build')) {
+        // proxy to the backend
+        if (ENABLE_KEY_CACHE) {
+            const response = await caches.default.match(event.request.url)
+            if (response) return response
+        }
+        const r = await fetch(`${BACKEND_URL}${url.pathname}`)
+        if (ENABLE_KEY_CACHE) {
+            const [b1, b2] = r.body.tee()
+            event.waitUntil(
+                caches.default.put(event.request.url, new Response(b1))
+            )
+            return new Response(b2, r)
+        } else {
+            return r
+        }
+    }
+    const spaKeyModifier = (request) => {
+        const _url = new URL(request.url)
+        if (_url.pathname.indexOf('.') === -1) {
+            // it's a SPA route
+            _url.pathname = '/'
+            return mapRequestToAsset(new Request(_url.toString(), request))
+        } else {
+            return mapRequestToAsset(request)
+        }
+    }
     const asset = await getAssetFromKV(event, {
         bypassCache: !ENABLE_ASSET_CACHE,
+        mapRequestToAsset: spaKeyModifier,
     })
     let body = asset.body
     if (ENABLE_ASSET_CACHE) {
