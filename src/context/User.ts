92939d4d46aa8c652e1f11815eb07f8211cfad67
Chance Hudson <jchancehud@gmail.com>
2022-04-17 18:41:10 +0200

    fix: load epks after signup and epoch transition

--- src/context/User.ts
+++ src/context/User.ts
@@ -251,12 +251,11 @@ export class User extends Synchronizer {
     }
 
     async checkInvitationCode(invitationCode: string): Promise<boolean> {
-        return true
         // check the code first but don't delete it until we signup --> related to backend
         const apiURL = makeURL(`genInvitationCode/${invitationCode}`, {})
         const r = await fetch(apiURL)
         if (!r.ok) return false
-        return r.json()
+        return true
     }
 
     private async _hasSignedUp(identity: string) {
@@ -316,6 +315,7 @@ export class User extends Synchronizer {
             throw error
         }
         await config.DEFAULT_ETH_PROVIDER.waitForTransaction(transaction)
+        await this.calculateAllEpks()
         // start the daemon later so the signup ui isn't slow
         this.startDaemon()
         return {
@@ -458,6 +458,7 @@ export class User extends Synchronizer {
     async epochEnded(event: any) {
         await super.epochEnded(event)
         await this.loadReputation()
+        await this.calculateAllEpks()
         this.spent = 0
     }
 }
