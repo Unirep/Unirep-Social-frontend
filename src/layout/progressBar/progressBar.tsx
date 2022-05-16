1a612ee3df057dbbbf19381b3282c8ce7722f49a
yu-zhen <wlphg010688@gmail.com>
2022-05-16 15:27:18 +0200

    add details list on progress bar

--- src/layout/progressBar/progressBar.tsx
+++ src/layout/progressBar/progressBar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import UserContext from '../../context/User'
@@ -8,9 +8,14 @@ const ProgressBar = () => {
     const userContext = useContext(UserContext)
     const queueContext = useContext(QueueContext)
 
+    const [isListOpen, setIsListOpen] = useState<boolean>(false)
+
     return (
-        <div className="progress-block">
-            {queueContext.loadingState !== LoadingState.none ? (
+        <div
+            className="progress-block"
+            onClick={() => setIsListOpen(!isListOpen)}
+        >
+            {queueContext.loadingState === LoadingState.loading ? (
                 <div className="progress-bar-container">
                     <div className="progress-bar">
                         <div
@@ -28,13 +33,48 @@ const ProgressBar = () => {
                         {Math.floor(userContext.syncPercent)}%
                     </div>
                 </div>
-            ) : (
-                <div className="progress-bar-container">Nothing's Pending.</div>
-            )}
+            ) : null}
             {queueContext.loadingState === LoadingState.loading ? (
-                <div className="progress-info">
-                    <h4>{queueContext.status.title}</h4>
-                    <p>{queueContext.status.details}</p>
+                <div
+                    className="progress-info"
+                    onClick={() => setIsListOpen(!isListOpen)}
+                >
+                    <div>
+                        <h4>{queueContext.status.title}</h4>
+                        <p>{queueContext.status.details}</p>
+                    </div>
+                    <h4>
+                        Detail{' '}
+                        <img
+                            src={require(`../../../public/images/arrow-${
+                                isListOpen ? 'up' : 'down'
+                            }-s-fill.svg`)}
+                        />
+                    </h4>
+                </div>
+            ) : (
+                <div
+                    className="progress-info"
+                    onClick={() => setIsListOpen(!isListOpen)}
+                >
+                    <h4>All done.</h4>
+                    <h4>
+                        Detail{' '}
+                        <img
+                            src={require(`../../../public/images/arrow-${
+                                isListOpen ? 'up' : 'down'
+                            }-s-fill.svg`)}
+                        />
+                    </h4>
+                </div>
+            )}
+            {isListOpen ? (
+                <div className="progress-list">
+                    {queueContext.histories.map((h, i) => (
+                        <div className="list-item" key={i}>
+                            {h.type}
+                        </div>
+                    ))}
                 </div>
             ) : null}
         </div>
