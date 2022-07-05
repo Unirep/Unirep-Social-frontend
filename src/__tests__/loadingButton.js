335ad38941c75222a4a629c9e96bc41f7b00a957
Anthony Madia <aamadia24@gmail.com>
2022-07-05 12:26:34 -0400

    Add/unit test for loadingButton

---
+++ src/__tests__/loadingButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { screen, render, rerender } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import LoadingButton from '../components/loadingButton/loadingButton'
+
+test('loadingButton renders and props render conditionally', () => {
+    render(<LoadingButton isLoading={false} name={"string"} />)
+    screen.debug()
+    expect(screen.getByText(/string/i)).toBeInTheDocument()
+    
+    render(<LoadingButton isLoading={true} name={"name"} />)
+    screen.debug()
+    expect(screen.getByText(/name/i)).toBeInTheDocument()
+})
